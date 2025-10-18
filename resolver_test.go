06732a63bb22ea22ebdcec26899ef63b757e9fbd
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-10-19 00:55:38 +0200

    test with grpc dialer

--- resolver_test.go
+++ resolver_test.go
@@ -2,12 +2,17 @@ package dnsdialer
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 func TestDialer_DialContext(t *testing.T) {
@@ -60,4 +65,49 @@ func TestDialer_DialContext_HTTPClient(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	resp.Body.Close()
+}
+
+func TestDialer_DialContext_gRPC(t *testing.T) {
+	// Create a simple gRPC server
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	assert.NoError(t, err)
+	defer listener.Close()
+
+	server := grpc.NewServer()
+
+	// Start the server in a goroutine
+	go func() {
+		server.Serve(listener)
+	}()
+	defer server.Stop()
+
+	dialer := New(
+		WithResolvers("8.8.8.8:53"),
+		WithStrategy(Race{}),
+	)
+
+	// Create gRPC connection using our custom dialer
+	conn, err := grpc.NewClient(
+		listener.Addr().String(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+			return dialer.DialContext(ctx, "tcp", addr)
+		}),
+	)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, conn)
+
+	// Test that the connection is actually working
+	// Since we don't have any services registered, we expect an unimplemented error
+	// which proves the connection is working
+	ctx := context.Background()
+	err = conn.Invoke(ctx, "/test.Service/Method", nil, nil)
+
+	// Should get "unimplemented" status, which means connection worked
+	st, ok := status.FromError(err)
+	assert.True(t, ok)
+	assert.Equal(t, codes.Unimplemented, st.Code())
+
+	conn.Close()
 }
