9f7fe3cada09afb2dc9dc0cfd14da00d8bed7ac4
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-10-19 23:13:00 +0200

    add copyright header

--- doc.go
+++ doc.go
@@ -1,3 +1,8 @@
+// Copyright 2025 Bruno Schaatsbergen. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package dnsdialer provides a drop-in replacement for net.Dialer with configurable
 // DNS resolution strategies.
 //
